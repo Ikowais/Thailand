dc1eca25a84d2e943c74d4c108314932a15469b8
Ikowais <165397256+Ikowais@users.noreply.github.com>
2025-01-15 14:48:40 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -30,5 +30,21 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
+const notificationList = document.getElementById("notification-list");
 
+notifications.forEach(notification => {
+    const item = document.createElement("div");
+    item.className = "notification-item";
+
+    item.innerHTML = `
+        <div class="icon">${notification.icon}</div>
+        <div class="text">
+            <h4>${notification.title}</h4>
+            <p>${notification.description}</p>
+        </div>
+        <div class="timestamp">${notification.timestamp}</div>
+    `;
+
+    notificationList.appendChild(item);
+});
 export default App;
